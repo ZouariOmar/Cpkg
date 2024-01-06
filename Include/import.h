b6804bfeef3c29bcedf993fdf1f1e82eb909d8f6
Zouari Omar <zouariomar20@gmail.com>
2024-01-06 17:14:49 +0100

    Divide the .h files

---
+++ Include/import.h
@@ -0,0 +1,19 @@
+// by_zouari_omar_full_stack_c++_dev
+// dd/mm/yyyy
+// Name Of Project Or Name The File
+#ifndef __IMPORT_H__
+#define __IMPORT_H__
+                    /* INCLUDE PROTOTYPE DECLARATION PART */
+// C INCLUDE SYS
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include <windows.h>
+// C INCLUDE CUSTOMIZED
+#include "msg.h"
+#include "color.h"
+#include "print.h"
+                    /* END OF INCLUDE PROTOTYPE DECLARATION PART */
+#endif
---
+++ Include/print.h
@@ -0,0 +1,13 @@
+// by_zouari_omar_full_stack_c++_dev
+// dd/mm/yyyy
+// Name Of Project Or Name The File
+#ifndef __PRINT_H__
+#define __PRINT_H__
+                    /* PRINT FUNCTIONS PROTOTYPE DECLARATION PART */
+// print_type ::: print the type of given variable
+#define printt(x) _Generic((x), \
+    int: printf("<int>\n", #x), \
+    float: printf("<float>\n", #x), \
+    default: printf("<Unknown>\n"))
+                    /* END OF PRINT FUNCTIONS PROTOTYPE DECLARATION PART  */
+#endif
