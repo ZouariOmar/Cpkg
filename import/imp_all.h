b11b6766e6e438d33957fdcab5dcaf9323e4e6f4
Zouari Omar <zouariomar20@gmail.com>
2024-06-04 12:11:12 +0100

    add imp_all.h

---
+++ import/imp_all.h
@@ -0,0 +1,27 @@
+/* -----------------------------------------------
+* @author:    @ZouariOmar
+* @IDE:       @vsc
+* @date:      mm/dd/yyyy
+? @file_name: imp_all.h
+----------------------------------------------- */
+
+#ifndef __IMP_ALL_H__
+#define __IMP_ALL_H__
+
+// ? ----------------------- INCLUDE PROTOTYPE DECLARATION PART -----------------------
+// * SYS INCLUDE LIBS
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+////#include <windows.h>
+
+// * CUSTOMIZED INCLUDE LIBS
+#include "../custom/inc/msg.h"
+#include "../custom/inc/color.h"
+#include "../func/print.h"
+#include "../func/inputV.h"
+
+#endif
